a2c251277dc94624adfbb65116b7282cb00fddf8
laurofraga <fragalauro12@gmail.com>
2025-05-29 10:09:43 -0300

    chore: add home pages

---
+++ hub-saude-frontend/src/app/pages/home-paciente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-paciente',
+  imports: [],
+  templateUrl: './home-paciente.component.html',
+  styleUrl: './home-paciente.component.scss'
+})
+export class HomePacienteComponent {
+
+}
