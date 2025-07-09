232358d47b3582bc111825f4b4ea09b7b0ab1f92
laurofraga <fragalauro12@gmail.com>
2025-07-09 15:44:39 -0300

    removing some coments

--- hub-saude-frontend/src/app/auth/user-data.resolver.ts
+++ hub-saude-frontend/src/app/auth/user-data.resolver.ts
@@ -3,17 +3,11 @@ import { ResolveFn } from '@angular/router';
 import { Observable, filter, take } from 'rxjs';
 import { AuthService, UsuarioLogado } from './auth.service';
 
-// Este resolver garante que a rota só será ativada quando houver um usuário logado.
 export const userDataResolver: ResolveFn<UsuarioLogado> = (route, state) => {
   const authService = inject(AuthService);
 
   return authService.usuarioAtual$.pipe(
-    // O 'filter' é a chave: ele ignora o valor inicial 'null' e espera
-    // até que o AuthService emita um objeto de usuário válido.
     filter((usuario): usuario is UsuarioLogado => usuario !== null),
-
-    // 'take(1)' garante que, assim que recebermos o primeiro usuário válido,
-    // o resolver completa e a navegação prossegue.
     take(1)
   );
 };
